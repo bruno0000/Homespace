6a7b1b9663d332694d910a13e62ce4a33f6558f0
bruno0000 <ounjq7@gmail.com>
2018-10-23 11:10:40 -0700

    added registration

--- app/src/main/java/com/example/homespace/homespace/MainActivity.java
+++ app/src/main/java/com/example/homespace/homespace/MainActivity.java
@@ -1,13 +1,121 @@
 package com.example.homespace.homespace;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private ProgressBar progressBarLogin;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBarLogin = findViewById(R.id.loginProgressBar);
+        progressBarLogin.setVisibility(View.INVISIBLE);
+        editTextUsername = findViewById(R.id.usernameEditText);
+        editTextPassword = findViewById(R.id.passwordEditText);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+        findViewById(R.id.registerButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
     }
+
+    private void createUser() {
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        if (username.isEmpty()) {
+            editTextUsername.setError("Username Required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextUsername.setError("Password Required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextUsername.setError("Password should be at least 6 characters long");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBarLogin.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(username, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            startHomeActivity();
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                userLogin(username, password);
+                            } else {
+                                progressBarLogin.setVisibility(View.INVISIBLE);
+                                Toast.makeText(MainActivity.this,
+                                        task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void userLogin(String username, String password) {
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            startHomeActivity();
+                        } else {
+                            progressBarLogin.setVisibility(View.INVISIBLE);
+                            Toast.makeText(MainActivity.this,
+                                    task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void startHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+
+    }
+
+
 }
