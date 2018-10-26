8df2ac8f8d928acff29800339a063a058fcfb42d
netugi <taraquad@gmail.com>
2018-10-26 00:49:52 -0700

    added button with calendar that pops up

--- app/src/main/java/com/example/homespace/homespace/TaskFragment.java
+++ app/src/main/java/com/example/homespace/homespace/TaskFragment.java
@@ -1,5 +1,6 @@
 package com.example.homespace.homespace;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class TaskFragment extends Fragment {
+
+    private static final String TAG = "TaskFragment";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tasks, null);
+        View v = inflater.inflate(R.layout.fragment_tasks, container, false);
+
+        Button buttonTestCalendar;
+        buttonTestCalendar = (Button) v.findViewById(R.id.buttonTestCalendar);
+
+        buttonTestCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 }
