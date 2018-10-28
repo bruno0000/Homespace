5ed1a9f42f1f8a7e347dbfefbd4dd5d853fe9680
netugi <taraquad@gmail.com>
2018-10-28 01:22:06 -0700

    Changed the login menu slightly to be in line with the orange styling added date and time for tasks (still need data to be sent to database though) changed overall color scheme for most things so far

--- app/src/main/java/com/example/homespace/homespace/TaskFragment.java
+++ app/src/main/java/com/example/homespace/homespace/TaskFragment.java
@@ -1,37 +1,102 @@
 package com.example.homespace.homespace;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 public class TaskFragment extends Fragment {
 
     private static final String TAG = "TaskFragment";
 
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener;
+    private int year, month, day, hour, minute;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_tasks, container, false);
 
-        Button buttonTestCalendar;
-        buttonTestCalendar = (Button) v.findViewById(R.id.buttonTestCalendar);
+        Button btnDatePicker;
+        Button btnTimePicker;
+        btnDatePicker = (Button) v.findViewById(R.id.buttonDatePicker);
+        btnTimePicker = (Button) v.findViewById(R.id.buttonTimePicker);
 
-        buttonTestCalendar.setOnClickListener(new View.OnClickListener() {
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), CalendarActivity.class);
-                startActivity(intent);
+                Calendar cal = Calendar.getInstance();
+                year = cal.get(Calendar.YEAR);
+                month = cal.get(Calendar.MONTH);
+                day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(getActivity(),
+                        android.R.style.Theme_DeviceDefault_Light,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.setCancelable(false);
+                dialog.show();
             }
         });
+
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog dialog = new TimePickerDialog(getActivity(),
+                        android.R.style.Theme_DeviceDefault_Light,
+                        mTimeSetListener,
+                        hour, minute,
+                        DateFormat.is24HourFormat(getActivity()));
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+                dialog.setCancelable(false);
+                dialog.show();
+            }
+        });
+
+        /*
+        * These next two listeners return the integer values for date and time
+        * May need to be formatted before put into the database
+        * */
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                String date = year + "/" + month + "/" + day;
+                Log.d("TaskFragment", "OnDateSet: year/month/day: " + date);
+            }
+        };
+
+        mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                String time = hour + ":" + minute;
+                Log.d("TaskFragment", "OnTimeSet: " + time);
+            }
+        };
+
         return v;
     }
 }
