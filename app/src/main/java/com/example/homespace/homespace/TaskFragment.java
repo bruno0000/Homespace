d8b6d95bb6d497ab2a0d2be3dc098ec51a540965
netugi <taraquad@gmail.com>
2018-10-28 18:32:10 -0700

    Date/Time now ready for storage in the database, as a Calendar object

--- app/src/main/java/com/example/homespace/homespace/TaskFragment.java
+++ app/src/main/java/com/example/homespace/homespace/TaskFragment.java
@@ -33,6 +33,7 @@ public class TaskFragment extends Fragment {
     private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TimePickerDialog.OnTimeSetListener mTimeSetListener;
+    private Calendar cal;
     private int year, month, day, hour, minute;
 
     @Nullable
@@ -42,13 +43,16 @@ public class TaskFragment extends Fragment {
 
         Button btnDatePicker;
         Button btnTimePicker;
+        Button btnDTConfirm;
+        cal = Calendar.getInstance();
+
         btnDatePicker = (Button) v.findViewById(R.id.buttonDatePicker);
         btnTimePicker = (Button) v.findViewById(R.id.buttonTimePicker);
+        btnDTConfirm = (Button) v.findViewById(R.id.buttonDTConfirm);
 
         btnDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Calendar cal = Calendar.getInstance();
                 year = cal.get(Calendar.YEAR);
                 month = cal.get(Calendar.MONTH);
                 day = cal.get(Calendar.DAY_OF_MONTH);
@@ -78,21 +82,36 @@ public class TaskFragment extends Fragment {
         });
 
         /*
-        * These next two listeners return the integer values for date and time
-        * May need to be formatted before put into the database
+        * Get the Calendar values for the database here
         * */
+        btnDTConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                cal.set(year, month, day, hour, minute);
+                Log.d("TaskFragment", "Cal.YEAR/MONTH/DAY_OF_MONTH/HOUR/MINUTE: " +
+                        cal.get(cal.YEAR) + "/" + cal.get(cal.MONTH) + "/" + cal.get(cal.DAY_OF_MONTH) +
+                        " " + cal.get(cal.HOUR) + ":" + cal.get(cal.MINUTE));
+            }
+        });
+
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                String date = year + "/" + month + "/" + day;
+            public void onDateSet(DatePicker datePicker, int yy, int mm, int dd) {
+                String date = yy + "/" + mm + "/" + dd;
+                year = yy;
+                month = mm;
+                day = dd;
                 Log.d("TaskFragment", "OnDateSet: year/month/day: " + date);
             }
         };
 
         mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-                String time = hour + ":" + minute;
+            public void onTimeSet(TimePicker timePicker, int hh, int mm) {
+                String time = hh + ":" + mm;
+                hour = hh;
+                minute = mm;
                 Log.d("TaskFragment", "OnTimeSet: " + time);
             }
         };
